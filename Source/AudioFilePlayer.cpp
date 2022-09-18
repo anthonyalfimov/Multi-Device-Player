0da1bd1899258f8c6ffe6feb794a8a34a9fa3166
Anthony Alfimov <Anthony.Alfimov@gmail.com>
2022-09-18 19:33:57 +0400

    Add AudioFilePlayer class

--- Source/AudioFilePlayer.cpp
+++ Source/AudioFilePlayer.cpp
@@ -9,3 +9,135 @@
 */
 
 #include "AudioFilePlayer.h"
+
+AudioFilePlayer::AudioFilePlayer()
+{
+    transportSource.addChangeListener (this);
+}
+
+//==============================================================================
+void AudioFilePlayer::setAudioFormatReader (AudioFormatReader* reader)
+{
+    if (reader == nullptr)      // if reader is not created, abort
+        return;
+
+    // Pass reader ownership to newSource:
+    auto newSource = std::make_unique<AudioFormatReaderSource> (reader, true);
+
+    // Set input source for the transportSource object:
+    transportSource.setSource (newSource.get(), 0, nullptr, reader->sampleRate);
+
+    // Transfer memory ownership of the audio source to readerSource ptr:
+    readerSource.reset (newSource.release());
+
+    // Update looping status of the new readerSource:
+    readerSource->setLooping (isLooping);
+
+    // Stop transport:
+    changeState (TransportState::Stopped);
+}
+
+//==========================================================================
+void AudioFilePlayer::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay (samplesPerBlockExpected, sampleRate);
+}
+
+void AudioFilePlayer::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+void AudioFilePlayer::getNextAudioBlock (const AudioSourceChannelInfo& info)
+{
+    transportSource.getNextAudioBlock (info);
+}
+
+//==============================================================================
+void AudioFilePlayer::playPause()
+{
+    if ((state == TransportState::Stopped) || (state == TransportState::Paused))
+        changeState (TransportState::Starting);
+    else if (state == TransportState::Playing)
+        changeState (TransportState::Pausing);
+}
+
+void AudioFilePlayer::stop()
+{
+    if (state == TransportState::Stopped)
+        return;
+
+    if (state == TransportState::Paused)
+        changeState (TransportState::Stopped);
+    else
+        changeState (TransportState::Stopping);
+}
+
+void AudioFilePlayer::setLooping (bool shouldLoop)
+{
+    isLooping = shouldLoop;
+
+    if (readerSource.get() != nullptr)
+        readerSource->setLooping (isLooping);
+}
+
+//==============================================================================
+void AudioFilePlayer::changeListenerCallback (ChangeBroadcaster* source)
+{
+    if (source != &transportSource)
+        return;
+
+    if (transportSource.isPlaying())
+        changeState (TransportState::Playing);
+    else if ((state == TransportState::Stopping) || (state == TransportState::Playing))
+        changeState (TransportState::Stopped);
+    else if (state == TransportState::Pausing)
+        changeState (TransportState::Paused);
+}
+
+//==============================================================================
+void AudioFilePlayer::changeState (TransportState newState)
+{
+    if (state == newState)
+        return;
+
+    state = newState;
+
+    switch (state)
+    {
+        case TransportState::Stopped:
+            transportSource.setPosition (0.0);
+
+            if (onTransportStopped != nullptr)
+                onTransportStopped();
+
+            break;
+
+        case TransportState::Starting:
+            transportSource.start();
+            break;
+
+        case TransportState::Playing:
+            if (onTransportStarted != nullptr)
+                onTransportStarted();
+
+            break;
+
+        case TransportState::Pausing:
+            transportSource.stop();
+            break;
+
+        case TransportState::Paused:
+            if (onTransportPaused != nullptr)
+                onTransportPaused();
+
+            break;
+
+        case TransportState::Stopping:
+            transportSource.stop();
+            break;
+
+        default:
+            break;
+    }
+}
--- Source/AudioFilePlayer.h
+++ Source/AudioFilePlayer.h
@@ -9,3 +9,66 @@
 */
 
 #pragma once
+
+#include <JuceHeader.h>
+
+class AudioFilePlayer  : public ChangeListener
+{
+public:
+    AudioFilePlayer();
+
+    //==========================================================================
+    // Load audio to play
+    void setAudioFormatReader (AudioFormatReader* reader);
+
+    //==========================================================================
+    // Audio processing
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate);
+
+    void releaseResources();
+
+    void getNextAudioBlock (const AudioSourceChannelInfo& info);
+
+    //==========================================================================
+    // Player transport controls
+    void playPause();
+    void stop();
+    void setLooping (bool shouldLoop);
+
+    //==========================================================================
+    // Player transport state
+    bool readyToPlay() const { return (readerSource != nullptr); }
+
+    enum class TransportState
+    {
+        Stopped,
+        Starting,
+        Playing,
+        Pausing,
+        Paused,
+        Stopping
+    };
+
+    TransportState getTransportState() const { return state; }
+    double getCurrentPosition() const { return transportSource.getCurrentPosition(); }
+
+    std::function<void()> onTransportStarted;
+    std::function<void()> onTransportPaused;
+    std::function<void()> onTransportStopped;
+
+    //==========================================================================
+    // Change listener callback
+    void changeListenerCallback (ChangeBroadcaster* source) override;
+
+private:
+    void changeState (TransportState newState);
+
+    bool isLooping = false;
+
+    std::unique_ptr<AudioFormatReaderSource> readerSource;
+    AudioTransportSource transportSource;
+    TransportState state = TransportState::Stopped;
+
+    //==========================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioFilePlayer);
+};
